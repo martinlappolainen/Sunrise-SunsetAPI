4d33546c0480ed6fc2f009e0789e31712d92739e
Opilane TTHK <opilane@tthk.ee>
2020-10-13 09:54:51 +0300

    Hes Work

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+
+
+namespace SunriseSunsetAPI
+{
+    class Program
+    {
+        public static string url = "https://api.sunrise-sunset.org/json?lat=59.4370&lng=24.7536&date=2020-10-13";
+        static void Main(string[] args)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+
+            using (var responsereader= new StreamReader(webStream))
+            {
+                var response = responsereader.ReadToEnd();
+                SunMode sunMode = JsonConvert.DeserializeObject<SunMode>(response);
+                Console.WriteLine($"Sunrise:{sunMode.Results.Sunrise}.");
+                Console.WriteLine($"Sunset:{sunMode.Results.Sunset}");
+                Console.WriteLine($"Status:{sunMode.Status}");
+            }
+        }
+    }
+}
---
+++ SunMode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SunriseSunsetAPI
+{
+    public class SunMode
+    {
+        public Results Results { get; set; }
+        public string Status { get; set; }
+    }
+    public class Results
+    {
+        public string Sunrise { get; set; }
+        public string Sunset { get; set; }
+
+    }
+       
+}
